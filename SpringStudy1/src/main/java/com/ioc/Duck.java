e47ff96426da29f2af89af83e5dc105b0c8a36b1
Doris-wr <2239456274@qq.com>
2020-05-06 23:39:38 +0800

    Spring学习

---
+++ SpringStudy1/src/main/java/com/ioc/Duck.java
@@ -0,0 +1,41 @@
+package com.ioc;
+
+/**
+ * @Author:wangrui
+ * @Date:2020/5/6 19:32
+ */
+public class Duck {
+    private String name;
+    private int age;
+
+    public Duck() {
+    }
+
+    public Duck(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Duck{"+
+                "name="+name+
+                ",age="+age+
+                "}";
+    }
+}
